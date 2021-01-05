8844aefc016b2ac2aedf551f7470112ab3aafe3d
Jue <me@jue.yt>
2021-01-05 20:48:36 +0100

    add static converter methods for customTypes

--- helper/serialization.js
+++ helper/serialization.js
@@ -4,21 +4,37 @@ const TAG = 32899;
 
 module.exports = ({customTypes} = {}) => {
 	if (!customTypes) customTypes = [];
-	const genTypes = customTypes.map((cls) => [cls, (gen, obj) => {
+	const genTypes = customTypes.map((Cls) => [Cls, (gen, obj) => {
 		// Get instance data
-		let objData = obj.toObject ? obj.toObject() : Object.assign({}, obj);
+		let objData;
+		if (Cls.toObject) {
+			objData = Cls.toObject(obj);
+		} else if (obj.toObject) {
+			objData = obj.toObject();
+		} else {
+			objData = obj;
+		}
 		// Make sure data is class-less. Otherwise this results in a endless-loop.
-		if (objData instanceof cls) objData = Object.assign({}, objData);
+		if (objData instanceof Cls) objData = Object.assign({}, objData);
 		// Push data into CBOR stream
-		return gen._pushTag(TAG) && gen.pushAny([cls.name, objData]);
+		return gen._pushTag(TAG) && gen.pushAny([Cls.name, objData]);
 	}]);
-	const clsByName = Object.fromEntries(customTypes.map((cls) => [cls.name, cls]));
+	const clsByName = Object.fromEntries(customTypes.map((Cls) => [Cls.name, Cls]));
 	const tags = Object.fromEntries([[TAG, ([name, objData]) => {
+		const Cls = clsByName[name];
+		if (Cls.fromObject) {
+			// Let the static fromObject helper create the class
+			return Cls.fromObject(objData);
+		}
+
 		// Create new instance
-		const obj = new clsByName[name]();
+		const obj = new Cls();
 		// Assign data from CBOR object
-		if (obj.fromObject) obj.fromObject(objData);
-		else Object.assign(obj, objData);
+		if (obj.fromObject) {
+			obj.fromObject(objData);
+		} else {
+			Object.assign(obj, objData);
+		}
 		return obj;
 	}]]);
 
